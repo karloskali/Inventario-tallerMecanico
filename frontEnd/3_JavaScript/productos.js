df71ee56e8c24c5fa73d5b679df0f9b5834080ad
Ramiro Mendivelso <ramirin600@hotmail.com>
2024-10-25 16:43:08 -0500

    enlace back end y front end de listar usuarios y actualizacion de la barra de navegacion

--- frontEnd/3_JavaScript/productos.js
+++ frontEnd/3_JavaScript/productos.js
@@ -1,11 +1,28 @@
-const modal = document.getElementById("modal");
+let urlServicioWebProductos = "http://localhost:8081/api/producto";
+let contenido_productos = document.querySelector("#listado_productos");
 
-window.addEventListener("click", (e) => {
-  if (e.target.matches(".btn-warning")) {
-    modal.classList.toggle("translate");
-  }
+function listar_productos() {
+    console.log("listar_productos");
+    let data = [];
+    let resultado = function (response) {
+        let items = [];
+        $.each(response, function(index, producto) {
+            items.push("<tr><td>" + producto.id + "</td><td>" + producto.nombre + "</td><td>" + producto.cantidad + "</td><td>" + producto.descripcion + "</td><td>" + producto.marca + "</td><td>" + producto.modelo + "</td><td>" + producto.precio + "</td><td>" + producto.ubicacion + "</td><td>" + producto.stock + "</td><td><a class='btn btn-success' href='editar.html?id=" + producto.id + "'>Editar</a></td><td><a class='btn btn-danger' href='eliminar.html?id=" + producto.id + "'>Eliminar</a></td></tr>");
+        });
+        
+        contenido_productos.innerHTML = "<table class='table table-striped table-bordered w-100'><thead><tr><th>ID</th><th>Nombre</th><th>Cantidad</th><th>Descripción</th><th>Marca</th><th>Modelo</th><th>Precio</th><th>Ubicación</th><th>Stock</th><th colspan='2'>Opciones</th></tr></thead><tbody>" + items.join("") + "</tbody></table>";
+    };
 
-  if (e.target.matches(".btn-danger")) {
-    modal.classList.toggle("translate");
-  }
-});
+    $.ajax({
+        type: "GET",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        url: urlServicioWebProductos,
+        context: data,
+        data: data,
+        dataType: "json",
+        success: resultado
+    });
+}
